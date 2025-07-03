d1cc1bf89e57e9cc5189d2544b0f06b0ff3ad22c
jery123 <jerykhenson@gmail.com>
2025-07-03 17:55:58 +0530

    Code reorganisation

---
+++ 01-spring-boot-overview/02-dev-tools-demo/mycoolapp/src/main/java/com/popcode/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,25 @@
+package com.popcode.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose '/' that return 'Hello world'
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    //expose a new endpoint for workout
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "My daily 5k!";
+    }
+
+    //expose a new component for fortune
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day.";
+    }
+}
