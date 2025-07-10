8abf01d8f354caa6e17d1a97b84460ec9ad4079d
jery123 <85230959+jery123@users.noreply.github.com>
2025-07-10 17:38:51 +0530

    saving

---
+++ 01-spring-boot-overview/06-properties-demo/src/test/java/com/popcode/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popcode.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
