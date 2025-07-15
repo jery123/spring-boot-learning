121099de7aeff38984e826cc0e85962feee9f073
jery123 <jerykhenson@gmail.com>
2025-07-15 18:30:22 +0530

    component scanning

---
+++ 02-spring-core-demo/02-component-scanning/src/main/java/com/popcode/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.popcode.springcoredemo.rest;
+
+
+import com.popcode.springcoredemo.common.Coach;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a pricete field for dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency
+    public DemoController(Coach theCoach){
+        myCoach = theCoach;
+    }
+
+    // define the
+    @GetMapping("/dailyworkout")
+    public String getDaiyWorkou(){
+        return myCoach.getDailyWorkout();
+    }
+
+}
