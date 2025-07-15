0db6cfdf4765b1e65e871af70eb3683dedf34a59
jery123 <85230959+jery123@users.noreply.github.com>
2025-07-15 18:30:22 +0530

    component scanning

---
+++ 02-spring-core-demo/02-component-scanning/src/main/java/com/popcode/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.popcode.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowing for 15 min";
+    }
+}
