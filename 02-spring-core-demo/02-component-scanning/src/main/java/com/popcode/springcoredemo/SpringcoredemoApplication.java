ec5a14959d31f376657f442fdb77508affc99fd0
jery123 <85230959+jery123@users.noreply.github.com>
2025-07-16 05:28:26 +0530

    Component scanning part 2

--- 02-spring-core-demo/02-component-scanning/src/main/java/com/popcode/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-core-demo/02-component-scanning/src/main/java/com/popcode/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,9 @@ package com.popcode.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication(
+//		scanBasePackages={"com.popcode.springcoredemo",
+//				          	"com.popcode.util"})
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
