43986aed591cef12192adad28b174a235961966a
jery123 <85230959+jery123@users.noreply.github.com>
2025-11-21 04:06:59 +0530

    Adding another exception handler

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/pop2c/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/pop2c/demo/rest/StudentRestController.java
@@ -62,4 +62,20 @@ public class StudentRestController {
         // return ResponseEntity
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    // add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
+
+        // Create a studentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
