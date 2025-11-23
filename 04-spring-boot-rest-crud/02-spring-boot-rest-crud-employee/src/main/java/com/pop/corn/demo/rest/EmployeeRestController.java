93a0a1626f52544de40626a69ac4a82da46a9956
jery123 <jerykhenson@gmail.com>
2025-11-24 05:05:19 +0530

    coding part of employee rest DAO

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/pop/corn/demo/rest/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package com.pop.corn.demo.rest;
+
+import com.pop.corn.demo.dao.EmployeeDAO;
+import com.pop.corn.demo.entity.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    // quick and dirty: inject employee dao
+    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    // expose "/employees" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
