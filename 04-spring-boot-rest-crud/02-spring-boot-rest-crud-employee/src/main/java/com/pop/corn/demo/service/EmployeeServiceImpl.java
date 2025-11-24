5add606d39ee36923329ed8245d7f72960206283
jery123 <85230959+jery123@users.noreply.github.com>
2025-11-25 05:03:16 +0530

    Service Layer implementation

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/pop/corn/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pop.corn.demo.service;
+
+
+import com.pop.corn.demo.dao.EmployeeDAO;
+import com.pop.corn.demo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO) {
+        employeeDAO = theEmployeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
