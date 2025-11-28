d5268bb0fb3a5b1e91ec4ce594690b24c6890348
jery123 <85230959+jery123@users.noreply.github.com>
2025-11-28 06:00:23 +0530

    REST method for patch Mapping

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/pop/corn/demo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/pop/corn/demo/rest/EmployeeRestController.java
@@ -5,8 +5,10 @@ import com.pop.corn.demo.entity.Employee;
 import com.pop.corn.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -27,9 +29,12 @@ public class EmployeeRestController {
      */
     private EmployeeService employeeService;
 
+    private JsonMapper jsonMapper;
+
     @Autowired
-    public EmployeeRestController(EmployeeService theEmployeeService) {
+    public EmployeeRestController(EmployeeService theEmployeeService, JsonMapper theJsonMapper) {
         employeeService = theEmployeeService;
+        jsonMapper = theJsonMapper;
     }
 
     // expose "/employees" and return a list of employees
@@ -61,5 +66,39 @@ public class EmployeeRestController {
         Employee dbEmployee = employeeService.save(employee);
 
         return dbEmployee;
+    }
+    
+    // add mapping for PUT /employees - update existing employee
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
+    // add mapping for PATCH /employee/{employeeId} - patch employee ... partial update
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload) {
+
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if(tempEmployee == null){
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        // theow exception if request body contains "id" key
+        if(patchPayload.containsKey("id")){
+            throw new RuntimeException("Employee id not allowed in request body");
+        }
+
+        Employee patchedEmployee = apply(patchPayload, tempEmployee);
+
+        return patchedEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
     }
 }
