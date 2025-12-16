11992a497d152268d91eeb86f8b3169e75ffb407
jery123 <jerykhenson@gmail.com>
2025-12-17 04:58:48 +0530

    starting with thymeleaf

---
+++ 06-sping-boot-mvc/01-thymeleafdemo-helloworld/src/test/java/com/pop2c/springboot/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pop2c.springboot.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
