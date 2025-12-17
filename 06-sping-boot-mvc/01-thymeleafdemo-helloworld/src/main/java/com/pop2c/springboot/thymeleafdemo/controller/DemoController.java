1740496c5c71800c6d8fb4b3bac55a42daec0043
jery123 <85230959+jery123@users.noreply.github.com>
2025-12-17 05:31:24 +0530

    My first thymeleaf view

---
+++ 06-sping-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/pop2c/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.pop2c.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+        // create a mapping for '/hello
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+        theModel.addAttribute("theDate", java.time.LocalDate.now());
+        theModel.addAttribute("theName", "Jery Foto");
+
+        return "helloworld";
+    }
+
+}
