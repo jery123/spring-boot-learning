6a66bc610aa5d0919e5bd81b07650489691cb567
jery123 <jerykhenson@gmail.com>
2025-12-25 04:26:28 +0530

    Load the list from  file

--- 06-sping-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/pop2c/springboot/thymeleafdemo/controller/StudentController.java
+++ 06-sping-boot-mvc/01-thymeleafdemo-helloworld/src/main/java/com/pop2c/springboot/thymeleafdemo/controller/StudentController.java
@@ -1,6 +1,7 @@
 package com.pop2c.springboot.thymeleafdemo.controller;
 
 import com.pop2c.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.Clock;
+import java.util.List;
 
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model theModel) {
@@ -20,6 +25,9 @@ public class StudentController {
 
         // add student object to the model attribute
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
