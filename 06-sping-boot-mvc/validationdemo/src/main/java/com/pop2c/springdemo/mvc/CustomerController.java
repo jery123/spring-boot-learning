442a877bba18b57ce141729c1eed623aa8a37055
jery123 <jerykhenson@gmail.com>
2025-12-29 03:58:00 +0530

    Adding the Controller to show HTML form

---
+++ 06-sping-boot-mvc/validationdemo/src/main/java/com/pop2c/springdemo/mvc/CustomerController.java
@@ -0,0 +1,16 @@
+package com.pop2c.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel){
+        theModel.addAttribute("customer", new Customer());
+
+        return "customer-form"; // return the actal name of the view
+    }
+}
