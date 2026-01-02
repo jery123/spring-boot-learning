4ee03c068da42e057a001ec24af41c91ba9a3386
jery123 <jerryp.dotpitchtechnologies@gmail.com>
2026-01-03 03:32:19 +0530

    Creation helper

---
+++ 06-sping-boot-mvc/validationdemo/src/main/java/com/pop2c/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,20 @@
+package com.pop2c.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+        boolean result = theCode.startsWith(coursePrefix);
+        return result;
+    }
+}
