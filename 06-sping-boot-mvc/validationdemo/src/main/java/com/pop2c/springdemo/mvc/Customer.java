d542e4b36ffeb6c29eb967c732c4137db9546151
jery123 <jerryp.dotpitchtechnologies@gmail.com>
2026-01-04 05:21:54 +0530

    Init the project

--- 06-sping-boot-mvc/validationdemo/src/main/java/com/pop2c/springdemo/mvc/Customer.java
+++ 06-sping-boot-mvc/validationdemo/src/main/java/com/pop2c/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.pop2c.springdemo.mvc;
 
+import com.pop2c.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -17,6 +18,17 @@ public class Customer {
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value="ABC", message = "must start with ABC")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getFirstName() {
         return firstName;
---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/pop2c/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.pop2c.springboot.cruddemo.dao;
+
+import com.pop2c.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
