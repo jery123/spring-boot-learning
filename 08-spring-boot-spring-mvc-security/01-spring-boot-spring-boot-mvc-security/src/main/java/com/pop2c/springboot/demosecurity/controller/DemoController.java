845298b79486faffac091f73052a00fb0970c3d7
jery123 <jerykhenson@gmail.com>
2026-01-10 05:14:57 +0530

    Spring mvc security setup

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-boot-mvc-security/src/main/java/com/pop2c/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.pop2c.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+}
