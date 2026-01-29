643dca6c785b4ee714bc3e99bda32cda645052cc
jery123 <85230959+jery123@users.noreply.github.com>
2026-01-30 05:17:47 +0530

    Init the command line App

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/pop2c/demo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/pop2c/demo/CruddemoApplication.java
@@ -1,7 +1,9 @@
 package com.pop2c.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -9,5 +11,15 @@ public class CruddemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] agrs) {
+
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
+
+
 
 }
