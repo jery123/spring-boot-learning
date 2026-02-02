7a5d08e120d8ac4d37464cd4abc61b2c1029a9a9
jery123 <85230959+jery123@users.noreply.github.com>
2026-02-03 00:28:57 +0530

    saving

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/pop2c/demo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/pop2c/demo/CruddemoApplication.java
@@ -1,6 +1,7 @@
 package com.pop2c.demo;
 
 import com.pop2c.demo.dao.AppDAO;
+import com.pop2c.demo.dao.AppDAOImpl;
 import com.pop2c.demo.entity.Instructor;
 import com.pop2c.demo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
@@ -16,19 +17,42 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(AppDAO theAppDAO) {
+	public CommandLineRunner commandLineRunner(AppDAO theAppDAO, AppDAOImpl appDAOImpl) {
 
 		return runner -> {
-			createInstructor(theAppDAO);
+//			createInstructor(theAppDAO);
+
+			findInstructor(theAppDAO);
 		};
 	}
 
+	private void findInstructor(AppDAO theAppDAO) {
+		int theId = 2;
+
+		System.out.println("Finding instructor id:" + theId);
+
+		Instructor tempInstructor = theAppDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetails only: " + tempInstructor.getInstructorDetail());
+	}
+
 	private void createInstructor(AppDAO theAppDAO) {
+		/*
 		Instructor instructor = new Instructor("Chad", "Darby", "darby@pop2c.com");
 
 		// create the instructor details
 		InstructorDetail tempInstructorDetail = new
 				InstructorDetail("https://www.youtube.com/@pop2c", "Coding");
+
+		// associate the objects
+		instructor.setInstructorDetail(tempInstructorDetail);*/
+
+		Instructor instructor = new Instructor("Madhu", "Patel", "patel@pop2c.com");
+
+		// create the instructor details
+		InstructorDetail tempInstructorDetail = new
+				InstructorDetail("https://www.youtube.com/@pop2c", "Playing guitar");
 
 		// associate the objects
 		instructor.setInstructorDetail(tempInstructorDetail);
