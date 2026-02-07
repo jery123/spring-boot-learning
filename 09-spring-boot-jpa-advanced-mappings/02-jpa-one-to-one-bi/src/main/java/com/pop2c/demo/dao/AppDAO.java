4e9a19b62ff25736b02025526685321bdd145e54
jery123 <85230959+jery123@users.noreply.github.com>
2026-02-08 04:29:54 +0530

    One-to-One Bi-Directional
    Creating a new project for One-to-One Bi-Directional, and adding the instructor property to the InstructorDetails class

---
+++ 09-spring-boot-jpa-advanced-mappings/02-jpa-one-to-one-bi/src/main/java/com/pop2c/demo/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package com.pop2c.demo.dao;
+
+import com.pop2c.demo.entity.Instructor;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+}
