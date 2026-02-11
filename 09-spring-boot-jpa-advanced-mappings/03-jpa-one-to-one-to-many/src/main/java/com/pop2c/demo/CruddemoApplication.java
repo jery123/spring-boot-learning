4ba2244aca3b4199adcbe3a7037a1c30ffb3155a
jery123 <85230959+jery123@users.noreply.github.com>
2026-02-11 05:39:07 +0530

    Setup one to many project

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-to-many/src/main/java/com/pop2c/demo/CruddemoApplication.java
@@ -0,0 +1,110 @@
+package com.pop2c.demo;
+
+import com.pop2c.demo.dao.AppDAO;
+import com.pop2c.demo.dao.AppDAOImpl;
+import com.pop2c.demo.entity.Instructor;
+import com.pop2c.demo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    private final AppDAOImpl appDAOImpl;
+
+    public CruddemoApplication(AppDAOImpl appDAOImpl) {
+        this.appDAOImpl = appDAOImpl;
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO theAppDAO, AppDAOImpl appDAOImpl) {
+
+		return runner -> {
+//			createInstructor(theAppDAO);
+
+//		 	findInstructor(theAppDAO);
+            
+//            deleteInstructor(theAppDAO);
+
+//			findInstructorDetail(theAppDAO);
+
+			deleteInstructorDetail(theAppDAO);
+		};
+	}
+
+	private void deleteInstructorDetail(AppDAO theAppDAO) {
+
+		int theId = 3;
+		System.out.println("Deleting the instructor detqil" + theId);
+
+		theAppDAO.deleteInstructorDetailById(theId);
+
+		System.out.println("Done !");
+	}
+
+	private void findInstructorDetail(AppDAO theAppDAO) {
+		int theId = 2;
+		System.out.println("Find the instructor detail by the id " + theId);
+
+		InstructorDetail tempInstructorDetail = theAppDAO.findInstructorDetailById(theId);
+
+		System.out.println("TempInstructorDetail" + tempInstructorDetail);
+		System.out.println("Instructor" + tempInstructorDetail.getInstructor());
+	}
+
+	private void deleteInstructor(AppDAO theAppDAO) {
+
+        int theId = 1;
+
+        System.out.println("Deleting instructor id:" + theId);
+
+        theAppDAO.deleteInstructorById(theId);
+        System.out.println("Done!");
+    }
+
+    private void findInstructor(AppDAO theAppDAO) {
+		int theId = 2	;
+
+		System.out.println("Finding instructor id:" + theId);
+
+		Instructor tempInstructor = theAppDAO.findInstructorById(theId);
+
+		System.out.println("tempInstructor: " + tempInstructor);
+		System.out.println("the associated instructorDetails only: " + tempInstructor.getInstructorDetail());
+	}
+
+	private void createInstructor(AppDAO theAppDAO) {
+		/*
+		Instructor instructor = new Instructor("Chad", "Darby", "darby@pop2c.com");
+
+		// create the instructor details
+		InstructorDetail tempInstructorDetail = new
+				InstructorDetail("https://www.youtube.com/@pop2c", "Coding");
+
+		// associate the objects
+		instructor.setInstructorDetail(tempInstructorDetail);*/
+
+		Instructor instructor = new Instructor("Madhu", "Patel", "patel@pop2c.com");
+
+		// create the instructor details
+		InstructorDetail tempInstructorDetail = new
+				InstructorDetail("https://www.youtube.com/@pop2c", "Playing guitar");
+
+		// associate the objects
+		instructor.setInstructorDetail(tempInstructorDetail);
+
+		//save the instructor
+
+		System.out.println("Saving instructor: " + instructor);
+		theAppDAO.save(instructor);
+		System.out.println("Done!");
+	}
+
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-to-many/src/main/java/com/pop2c/demo/dao/AppDAO.java
@@ -0,0 +1,17 @@
+package com.pop2c.demo.dao;
+
+import com.pop2c.demo.entity.Instructor;
+import com.pop2c.demo.entity.InstructorDetail;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+}
