8a6bb57abfaedbb5ffe2dccccebe53ae08ef5a47
jery123 <85230959+jery123@users.noreply.github.com>
2026-02-12 08:16:37 +0530

    Updating the course entity and apply the one to many relationship

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-to-many/src/main/java/com/pop2c/demo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-one-to-many/src/main/java/com/pop2c/demo/entity/Course.java
@@ -1,4 +1,61 @@
 package com.pop2c.demo.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="course")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="title")
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name="instructor_id")
+    private Instructor instructor;
+
+    public Course(int id, String title, Instructor instructor) {
+        this.id = id;
+        this.title = title;
+        this.instructor = instructor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", instructor=" + instructor +
+                '}';
+    }               
 }
