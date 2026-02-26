d162312cfebbb775f13cda17cf20f910fcb8a1de
jery123 <85230959+jery123@users.noreply.github.com>
2026-02-27 04:30:56 +0530

    Adding conveninece methods for the course in the Student Entity

--- 09-spring-boot-jpa-advanced-mappings/05-jpa-one-to-many-to-many/src/main/java/com/pop2c/demo/entity/Student.java
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-one-to-many-to-many/src/main/java/com/pop2c/demo/entity/Student.java
@@ -2,6 +2,9 @@ package com.pop2c.demo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="student")
 public class Student {
@@ -19,6 +22,15 @@ public class Student {
 
     @Column(name="email")
     private String email;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH,
+            },
+            mappedBy = "students"
+    )
+    private List<Course> courses;
 
     public Student(){}
 
@@ -58,6 +70,24 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    // add convenience method
+    public void addCourse(Course theCourse){
+        if(courses == null){
+            courses = new ArrayList<>();
+        }
+
+        courses.add(theCourse);
+        theCourse.addStudent(this);
     }
 
     @Override
