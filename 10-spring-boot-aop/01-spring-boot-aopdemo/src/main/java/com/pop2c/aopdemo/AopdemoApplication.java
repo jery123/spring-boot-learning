bd2d9af45bc0c0ed017f2f054da205230b3acccc
jery123 <85230959+jery123@users.noreply.github.com>
2026-03-05 04:59:38 +0530

    Get ready for the next step
    Setup of a next project for AOP

---
+++ 10-spring-boot-aop/01-spring-boot-aopdemo/src/main/java/com/pop2c/aopdemo/AopdemoApplication.java
@@ -0,0 +1,26 @@
+package com.pop2c.aopdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class AopdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopdemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
