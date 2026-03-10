3d4f45a5b3c174b132d7981c5941ea0439f31cce
jery123 <85230959+jery123@users.noreply.github.com>
2026-03-11 04:56:44 +0530

    Adding an additional @Before statement by reusing the @Pointcut declaration

--- 10-spring-boot-aop/02-spring-boot-aopdemo-ponitcut-declarations/src/main/java/com/pop2c/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aopdemo-ponitcut-declarations/src/main/java/com/pop2c/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,73 +12,7 @@ import org.springframework.stereotype.Component;
 @SpringBootApplication(exclude = JmxAutoConfiguration.class)
 public class MyDemoLoggingAspect {
 
-    // this is where we add all of our related advices for logging
-
-    // let's start with an @Before advice
-
-
-
-    /*@Before("execution(public void updateAccount())")
-    public void beforeAddAccountAdvice() {
-
-        System.out.println("\n======> Executing @Before advice on method <======");
-
-    }*/
-
-    // Apply the @Before on any addAccount method
-    /*@Before("execution(public void addAccount())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-
-    // Apply the @Before on addAccount method in AccountDAO class only
-    /*@Before("execution(public void com.pop2c.aopdemo.dao.AccountDAO.addAccount())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-
-    // Apply the @Before on any method starting with add
-   /* @Before("execution(public void add*())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-
-    // Apply the @Before on any method starting with add and with a return type of void
-    /*@Before("execution(void add*())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-
-    // Apply the @Before on any method starting with add and with any return type
-    /*@Before("execution(* add*())")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-    // Apply the @Before on any method starting with add and with any return type, having param of Account
-    /*@Before("execution(* add*(com.pop2c.aopdemo.Account))")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-    // Match method with `Account` parameter follow by more param types
-    /*@Before("execution(* add*(com.pop2c.aopdemo.Account, ..))")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-    // Match on method with any parameter
-    /*@Before("execution(* add*(..))")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======> Executing @Before advice on addAccount() <======");
-    }*/
-
-
-    //Creating the Poincut expression
+    // Creating the Poincut expression
     @Pointcut("execution(* com.pop2c.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {}
 
@@ -86,5 +20,11 @@ public class MyDemoLoggingAspect {
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n======> Executing @Before advice on addAccount() <======");
+    }
+
+    // New advice
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
     }
 }
