0c53f02b1e590dbd70b4d363d855957ba6b92462
jery123 <85230959+jery123@users.noreply.github.com>
2026-03-27 14:49:59 +0530

    Added the Aspect dependency and init the aspect class

---
+++ 10-spring-boot-aop/spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -0,0 +1,10 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import java.util.logging.Logger;
+
+public class DemoLoggingAspect {
+
+    //setup logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+    
+}
